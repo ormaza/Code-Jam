6159377610186cfabe70fdbc9a06f63f47462f15
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2015-10-12 03:37:48 -0300

    Create SRM 191 Div 2 Easy – BettingMoney.cs

---
+++ TopCoder/SRM 191 Div 2 Easy – BettingMoney.cs
@@ -0,0 +1,16 @@
+public class BettingMoney
+    {
+        public int moneyMade(int[] amounts, int[] centsPerDollar, int finalResult)
+        {
+            int gain = 0;
+            for (int i = 0; i < amounts.Length;i++ )
+            {
+                if (i == finalResult) continue;
+                else gain += amounts[i];
+            }
+            gain*=100;
+            int loss = centsPerDollar[finalResult]*amounts[finalResult];
+            return gain-loss;
+        }
+
+    }
