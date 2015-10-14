97e59c8a016289dee285b9abc2b82f235160a107
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2015-10-13 23:28:30 -0300

    Update SRM 146 Div 2 Easy – Yahtzee.cpp

--- TopCoder/SRM 146 Div 2 Easy – Yahtzee.cpp
+++ TopCoder/SRM 146 Div 2 Easy – Yahtzee.cpp
@@ -1,15 +1,3 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <vector>
-using namespace std;
-
 class YahtzeeScore{
   public: int maxPoints(vector <int> toss){
 	  sort(toss.begin(), toss.end());
