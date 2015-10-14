d9878bb6fdb3e5d2317e810d032263f6ca879005
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2015-10-13 23:21:36 -0300

    Create SRM 185 Div 2 Easy – PassingGrade.cpp

---
+++ TopCoder/SRM 185 Div 2 Easy – PassingGrade.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+class PassingGrade
+{
+    public: int pointsNeeded(vector <int> pointsEarned, vector <int> pointsPossible, int finalExam)
+    {
+        float reach=finalExam;
+        for(int i=0;i<pointsPossible.size();i++) reach+=pointsPossible[i];
+        reach*=0.65;
+
+        float earned=0;
+        for(int i=0;i<pointsEarned.size();i++) earned+=pointsEarned[i];
+        float need=(reach-earned);
+
+        if(need>finalExam) return -1;
+        else if(need<=0) return 0;
+        else return ceil(need);
+    }
+};
