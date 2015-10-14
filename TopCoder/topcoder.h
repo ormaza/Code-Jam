18fe384ffd84502edefdfb2e03593bdfe2f15e15
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2015-10-13 23:29:01 -0300

    Create topcoder.h

---
+++ TopCoder/topcoder.h
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
