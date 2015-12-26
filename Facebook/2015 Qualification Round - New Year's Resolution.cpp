b4b5e10db10851236d05e3d1eb0fdfbb38e688f7
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2015-12-26 14:43:23 -0300

    Create 2015 Qualification Round - New Year's Resolution.cpp

---
+++ Facebook/2015 Qualification Round - New Year's Resolution.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool Verify(int p[],int c[],int f[], int n, int P,int C,int F)
+{
+    if(P==0 && C==0 && F==0) return true;
+    if(n==0 && (P!=0||C!=0||F!=0)) return false;
+
+    if (p[n-1] > P || c[n-1] > C || f[n-1] > F ) return Verify(p,c,f,n-1,P,C,F);
+
+    return Verify(p,c,f, n-1, P,C,F) || Verify(p,c,f, n-1, P-p[n-1],C-c[n-1],F-f[n-1]);
+}
+
+int main()
+{
+    int t; cin>>t;
+    int cont=1;
+    while(t--)
+    {
+        int P,C,F;
+        cin>>P>>C>>F;
+
+        int n; cin>>n;
+        int p[n],c[n],f[n];
+
+        for(int i=0;i<n;i++) cin>>p[i]>>c[i]>>f[i];
+
+        if(Verify(p,c,f,n,P,C,F)) cout<<"Case #"<<cont++<<": yes"<<endl;
+        else cout<<"Case #"<<cont++<<": no"<<endl;
+    }
+  return 0;
+}
