973e3bd886b0c5dfb5364ac9fab908749b8c99bf
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2016-02-16 01:11:07 -0300

    Create 6821 - Automated Checking Machine.cpp

---
+++ UVA/6821 - Automated Checking Machine.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x[5],y[5];
+
+    while(cin>>x[0]>>x[1]>>x[2]>>x[3]>>x[4]>>y[0]>>y[1]>>y[2]>>y[3]>>y[4])
+    {
+        bool verif=true;
+
+         for(int i=0;i<5;i++)
+        {
+            if(y[i]==x[i])
+            {
+                cout<<"N"<<endl;
+                verif=false;
+                break;
+            }
+        }
+        if(verif)cout<<"Y"<<endl;
+    }
+
+    return 0;
+}
