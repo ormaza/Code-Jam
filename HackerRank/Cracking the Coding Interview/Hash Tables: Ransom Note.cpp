692f2cc6d9246cd24aa273db0a500ac1b917a4a6
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2018-03-12 18:09:18 -0300

    Create Hash Tables: Ransom Note.cpp

---
+++ HackerRank/Cracking the Coding Interview/Hash Tables: Ransom Note.cpp
@@ -0,0 +1,63 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    map<string, int> words;
+    string tmp;
+
+    for(int i=0;i<magazine.size();i++){
+        tmp = magazine[i];
+        if(words.count(tmp)) words[tmp]++;
+        else words.insert(make_pair(tmp,1));
+    }
+
+    for(int i=0;i<ransom.size();i++){
+        tmp = ransom[i];
+        if(words.count(tmp)){
+            if(words[tmp]) words[tmp]--;
+            else return false;
+        } else return false;
+    }
+    return true;
+}
+
+int main(){
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
