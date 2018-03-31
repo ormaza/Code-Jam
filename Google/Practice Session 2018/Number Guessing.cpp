bbbb0bd058fda91de9f1476e6d3797056b6bd0af
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2018-03-31 13:50:29 -0300

    Create Number Guessing.cpp

---
+++ Google/Practice Session 2018/Number Guessing.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<cstdio>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    float a,b,x; //A<x<=B
+    float ans;
+    int n; //n=guesses
+    string judge="";
+    while(t--){
+        cin>>a>>b>>n;
+        while(n--){
+            ans=(b-a)/2;
+            x=a+ceil(ans);
+            cout<<x<<endl;
+            cin>>judge;
+
+            if(judge=="TOO_BIG") b=x;
+            else if(judge=="TOO_SMALL") a=x;
+            else if(judge=="WRONG_ANSWER") return 0;
+            else break;
+
+        }
+    }
+    return 0;
+}
