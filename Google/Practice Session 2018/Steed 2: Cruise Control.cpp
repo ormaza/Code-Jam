0101b713d90570cd43bbbcbf900e424c3a6f8d31
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2018-03-31 22:03:32 -0300

    Wrong answer

---
+++ Google/Practice Session 2018/Steed 2: Cruise Control.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<cstdio>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    int t,n; cin>>t;
+    double d,kmax,k,calc,ans,s;
+    for(int caso=1;caso<=t;caso++){
+       kmax = 0;
+       cin>>d>>n;
+       while(n--){
+            cin>>k>>s;
+            calc = (d-k)/s;
+            kmax = max(calc,kmax);
+       }
+       ans = d/kmax;
+       cout<<"Case #"<<caso<<": "<<ans<<endl;
+    }
+    return 0;
+}
